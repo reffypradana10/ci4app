c84a17805be0bb416444e234c15ee6903a4b18a2
reffypradana10 <reffypradana10@gmail.com>
2024-11-14 20:50:09 +0700

    Yang penting commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,12 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/coba/index', 'Coba::index');
-$routes->get('/coba/about', 'Coba::about');
-$routes->get('/coba/about/(:any)', 'Coba::about/$1');
-$routes->get('/coba/(:any)', 'Coba::about/$1');
+$routes->get('/', 'Pages::index');
 
-$routes->get('/users', 'Admin\Users::index');
 
 $routes->setAutoRoute(true);
---
+++ app/Views/pages/about.php
@@ -0,0 +1,2 @@
+<h1>About Me</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ad nisi asperiores debitis? Asperiores non quasi placeat quidem. Deserunt, voluptas!</p>
